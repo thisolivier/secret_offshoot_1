ce572af78664899335df9e1bdb64054d60b42bf0
Olivier Butler <olivier.butler@gmail.com>
2017-11-14 14:43:11 +0000

    Controller written (MVP), waiting for simulator to boot.

--- Five/Five/ViewController.swift
+++ Five/Five/ViewController.swift
@@ -16,6 +16,8 @@ import UIKit
  
  Optional: 
  Consider how the icons might be displayed if the visible set doesn't fit the width of the screen
+ // Have to go, but would set listener for width failure
+ // On width failure, use a vertical stack, or break one item onto new line, or show '...' for toggle.
 
  */
 
@@ -25,9 +27,27 @@ class ViewController: UIViewController {
     var videoCount: Int = 3
     var photoCount: Int = 2
 
+    @IBOutlet weak var videoCounterOutlet: UILabel!
+    @IBOutlet weak var musicCounterOutlet: UILabel!
+    @IBOutlet weak var imageCounterOutlet: UILabel!
+    @IBOutlet weak var musicViewOutlet: UIView!
+    @IBOutlet weak var videoViewOutlet: UIView!
+    @IBOutlet weak var imageViewOutlet: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let counters = [musicCounterOutlet, videoCounterOutlet, imageCounterOutlet]
+        let inputs = [musicCount, videoCount, photoCount]
+        let views = [musicViewOutlet, videoViewOutlet, imageViewOutlet]
+        
+        for index in 0...2{
+            if inputs[index] == 0 {
+                views[index]?.isHidden = true
+            } else {
+                counters[index]?.text = String(inputs[index])
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
